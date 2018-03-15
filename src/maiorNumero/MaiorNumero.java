2d10e556ee0b0aaf6c4de0b2724bc90297aa01ab
Maycon <mayconn.aguiarr@gmail.com>
2018-03-15 17:24:33 -0300

    adicionado estrutura do método 'MaiorNumero' e classe de teste

--- src/maiorNumero/MaiorNumero.java
+++ src/maiorNumero/MaiorNumero.java
@@ -1,5 +1,21 @@
 package maiorNumero;
 
-public class MaiorNumero {
+public class MaiorNumero { 
 
+	public int maiorValor(int valor1, int valor2) {
+		if ((valor1 < 0) || valor2 < 0) {
+			throw new IllegalArgumentException("Não é permitido números negativos");
+		}
+		int maior = 0;
+		
+		if (valor1 > valor2) {
+			maior = valor1;
+		} else if (valor2 > valor1) {
+			maior = valor2;
+		}
+
+		System.out.println("O maior valor é :" + maior);
+
+		return maior;
+	}
 }
---
+++ testes/javaExercisesWithTests/MaiorValorTeste.java
@@ -0,0 +1,34 @@
+package javaExercisesWithTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import maiorNumero.MaiorNumero;
+
+public class MaiorValorTeste {
+
+	private MaiorNumero maiorNumero = new MaiorNumero();
+
+	@Test
+	public void deveMostrarNumeroMaiorDaEsquerda() {
+		int expected = 6;
+		assertEquals(expected, maiorNumero.maiorValor(6, 5));
+	}
+
+	@Test
+	public void deveMostrarNumeroMaiorDaDireita() {
+		int expected = 10;
+		assertEquals(expected, maiorNumero.maiorValor(6, 10)); 
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void deveMostrarUmaExcecaoCasoUmNumeroForNegativo() {
+		maiorNumero.maiorValor(5, -2);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void deveMostarUmaExceçãoParaDoisNumerosNegativos() {
+		maiorNumero.maiorValor(-5, -8);
+	}
+}
